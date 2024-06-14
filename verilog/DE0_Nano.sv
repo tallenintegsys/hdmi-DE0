85e1f6476a32430dff524e86ac6ce6d6996a58cd
Tim <tallen@integsys.biz>
2024-06-13 21:46:25 -0400

    I need a linter

--- verilog/DE0_Nano.sv
+++ verilog/DE0_Nano.sv
@@ -41,41 +41,27 @@ module DE0_Nano(
     output           [2:0]      HDMI_OUT,
     output                      HDMI_CLK);
 
+    assign LED = rgb[7:0];
+    wire clk_pixel_x5;
+    wire clk_pixel;
+    wire clk_audio;
+    hdmi_pll hdmi_pll(.inclk0(CLOCK_50), .c0(clk_pixel), .c1(clk_pixel_x5), .c2(clk_audio));
 
-//=======================================================
-//  PARAMETER declarations
-//=======================================================
+    localparam AUDIO_BIT_WIDTH = 16;
+    localparam AUDIO_RATE = 48000;
+    localparam WAVE_RATE = 480;
 
+    logic [AUDIO_BIT_WIDTH-1:0] audio_sample_word;
+    logic [AUDIO_BIT_WIDTH-1:0] audio_sample_word_dampened; // This is to avoid giving you a heart attack -- it'll be really loud if it uses the full dynamic range.
+    assign audio_sample_word_dampened = audio_sample_word >> 9;
 
-//=======================================================
-//  REG/WIRE declarations
-//=======================================================
+    //sawtooth #(.BIT_WIDTH(AUDIO_BIT_WIDTH), .SAMPLE_RATE(AUDIO_RATE), .WAVE_RATE(WAVE_RATE)) sawtooth (.clk_audio(clk_audio), .level(audio_sample_word));
 
-
-//=======================================================
-//  Structural coding
-//=======================================================
-
-wire clk_pixel_x5;
-wire clk_pixel;
-wire clk_audio;
-hdmi_pll hdmi_pll(.inclk0(CLOCK_50), .c0(clk_pixel), .c1(clk_pixel_x5), .c2(clk_audio));
-
-localparam AUDIO_BIT_WIDTH = 16;
-localparam AUDIO_RATE = 48000;
-localparam WAVE_RATE = 480;
-
-logic [AUDIO_BIT_WIDTH-1:0] audio_sample_word;
-logic [AUDIO_BIT_WIDTH-1:0] audio_sample_word_dampened; // This is to avoid giving you a heart attack -- it'll be really loud if it uses the full dynamic range.
-assign audio_sample_word_dampened = audio_sample_word >> 9;
-
-//sawtooth #(.BIT_WIDTH(AUDIO_BIT_WIDTH), .SAMPLE_RATE(AUDIO_RATE), .WAVE_RATE(WAVE_RATE)) sawtooth (.clk_audio(clk_audio), .level(audio_sample_word));
-
-logic [23:0] rgb;// = 24'd523700000000;
-logic [9:0] cx, cy;
-hdmi #( .VIDEO_ID_CODE(4),
-        .AUDIO_RATE(AUDIO_RATE),
-        .AUDIO_BIT_WIDTH(AUDIO_BIT_WIDTH))
+    logic [23:0] rgb;// = 24'd523700000000;
+    logic [9:0] cx, cy;
+    hdmi #( .VIDEO_ID_CODE(4),
+            .AUDIO_RATE(AUDIO_RATE),
+            .AUDIO_BIT_WIDTH(AUDIO_BIT_WIDTH))
         hdmi(.clk_pixel_x5(clk_pixel_x5),
              .clk_pixel(clk_pixel),
              .clk_audio(clk_audio),
@@ -86,28 +72,24 @@ hdmi #( .VIDEO_ID_CODE(4),
              .cx(cx),
              .cy(cy));
 
-logic [7:0] character = 8'h30;
-logic [5:0] prevcy = 6'd0;
-always @(posedge clk_pixel)
-begin
-    if (cy == 10'd0)
-    begin
-        character <= 8'h30;
-        prevcy <= 6'd0;
+    logic [7:0] character = 8'h30;
+    logic [5:0] prevcy = 6'd0;
+    always @(posedge clk_pixel) begin
+        if (cy == 10'd0) begin
+            character <= 8'h30;
+            prevcy <= 6'd0;
+        end else if (prevcy != cy[9:4]) begin
+            character <= character + 8'h01;
+            prevcy <= cy[9:4];
+        end
     end
-    else if (prevcy != cy[9:4])
-    begin
-        character <= character + 8'h01;
-        prevcy <= cy[9:4];
-    end
-end
 
-LFSR #(.NUM_BITS(24)) LSFR (
-   .i_Clk(CLOCK_50),
-   .i_Enable(1),
-   .o_LFSR_Data(rgb)
-   );
+    LFSR #(.NUM_BITS(24)) LSFR (
+       .i_Clk(CLOCK_50),
+       .i_Enable(1),
+       .o_LFSR_Data(rgb)
+    );
 
-//console console(.clk_pixel(clk_pixel), .codepoint(character), .attribute({cx[9], cy[8:6], cx[8:5]}), .cx(cx), .cy(cy), .rgb(rgb));
+    //console console(.clk_pixel(clk_pixel), .codepoint(character), .attribute({cx[9], cy[8:6], cx[8:5]}), .cx(cx), .cy(cy), .rgb(rgb));
 
 endmodule
